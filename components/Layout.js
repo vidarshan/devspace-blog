e5f672459d55d3ef63d07f1580d6818df2b4a01f
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-22 20:17:05 +0530

    Layout setup

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favion.ico" />
+      </Head>
+      <main className="container mx-auto my-7">{children}</main>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Welcome to DevSpace",
+  keywords: "development, coding, programming",
+  description: "Th best info and news in development",
+};
