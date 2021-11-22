49c321e26c4dfbedc604c374389054e776f6eb8e
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-22 22:09:36 +0530

    Showing posts in home

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,24 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Layout from "../components/Layout";
+import Post from "../components/Post";
+import Link from "next/link";
 
 export default function HomePage({ posts }) {
   console.log(posts);
   return (
     <Layout>
-      <h1>Hello</h1>
+      <h1 className="text-5xl border-b-4 p-5">Latest Posts</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          View All
+        </a>
+      </Link>
     </Layout>
   );
 }
