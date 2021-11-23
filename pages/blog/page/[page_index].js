8e63a88187614f923bbd689f4c726ce8eb13de8d
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-23 20:33:15 +0530

    Changes to blog pages

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -1,25 +1,18 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import Layout from "../components/Layout";
-import Post from "../components/Post";
-import Link from "next/link";
-import { sortByDate } from "../utils/index";
+import Layout from "../../../components/Layout";
+import Post from "../../../components/Post";
 
-export default function HomePage({ posts }) {
+export default function BlogPage({ posts }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5">Latest Posts</h1>
+      <h1 className="text-5xl border-b-4 p-5">Blog Posts</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
       </div>
-      <Link href="/blog">
-        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
-          View All
-        </a>
-      </Link>
     </Layout>
   );
 }
@@ -45,7 +38,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: posts.sort(sortByDate).slice(0, 6),
+      posts,
     },
   };
 }
