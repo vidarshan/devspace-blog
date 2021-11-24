5492c2ae026590d90e70d7b8aa42694157c04faf
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-24 11:21:25 +0530

    Purge css change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  purge: {
+    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
+  }
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
