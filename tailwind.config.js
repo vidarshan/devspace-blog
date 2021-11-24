b4c33622601d90d3bfdf554f5e852c88408fe62c
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-24 11:22:55 +0530

    Typo fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
-  }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
